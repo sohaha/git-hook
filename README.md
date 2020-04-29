cddb60c5b538521d7e4acb2694050bfc7ee3d771
seekwe <seekwe@gmail.com>
2020-04-29 13:28:49 +0800

    misc: ok(#master)

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # git-hook
 
-```bash
-git config --global alias.sethook '!f() { set -ex ; curl -sSL https://raw.githubusercontent.com/sohaha/git-hook/master/install.sh -o .install.sh ; chmod +x .install.sh ; mkdir -p .git/hooks ; ./.install.sh ; rm -f ./.install.sh ; }; f'
-```
+## 说明
+
+使用 git hook 实现提交前代码格式化与避免提交非项目文件等等操作。
+
+## 使用
+
+打开终端执行下面命令安装 git alias（ Windows 下请使用 git-bash ）
 
 ```bash
-git setHook
+git config --global alias.inithook '!f() { set -ex ; curl -sSL https://raw.githubusercontent.com/sohaha/git-hook/master/install.sh -o .install.sh ; chmod +x .install.sh ; mkdir -p .git/hooks ; ./.install.sh ; rm -f ./.install.sh ; }; f'
+```
+
+在 Git 仓库根目录执行初始化钩子
+
+```bash
+git inithook
 ```
--- install.sh
+++ install.sh
@@ -3,13 +3,19 @@
 # Introduce: https://github.com/sohaha/git-hook
 # Version:   v1.0.0
 
+isChinaProxy="https://github.73zls.com/"
+isChina=$(curl --silent "cip.cc" | grep "中国")
+if [ "" == isChina ]; then
+  isChinaProxy=""
+fi
+
 
 hookfile=$(git rev-parse --show-toplevel)/.git/hooks/pre-commit ; 
-curl -sSL https://raw.githubusercontent.com/sohaha/git-hook/master/pre-commit -o $hookfile ; 
+curl -sSL ${isChinaProxy}https://raw.githubusercontent.com/sohaha/git-hook/master/pre-commit -o $hookfile ; 
 chmod +x $hookfile ;
 
 commitookfile=$(git rev-parse --show-toplevel)/.git/hooks/commit-msg ; 
-curl -sSL https://raw.githubusercontent.com/sohaha/git-hook/master/commit-msg -o $commitookfile ; 
+curl -sSL ${isChinaProxy}https://raw.githubusercontent.com/sohaha/git-hook/master/commit-msg -o $commitookfile ; 
 chmod +x $commitookfile ;
 
 echo "install done"
